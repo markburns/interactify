7f53853174bd697fa5070310a455f1a9b0936e65
Mark Burns <markburns@users.noreply.github.com>
2023-12-29 14:31:21 +0000

    DRY up rspec matchers

--- lib/interactify/rspec/matchers.rb
+++ lib/interactify/rspec/matchers.rb
@@ -1,67 +1,68 @@
 # frozen_string_literal: true
 
-require "interactify/interactor_wiring"
+require "interactify/wiring"
 
-# Custom matcher that implements expect_inputs
-# e.g.
-# expect(described_class).to expect_inputs(:connection, :order)
+module Interactify
+  module RSpec
+    class ContractMatcher
+      attr_reader :actual, :expected_values, :actual_values, :type
 
-RSpec::Matchers.define :expect_inputs do |*expected_inputs|
-  match do |actual|
-    next false unless actual.respond_to?(:expected_keys)
+      def initialize(actual, expected_values, actual_values, type)
+        @actual = actual
+        @expected_values = expected_values
+        @actual_values = actual_values
+        @type = type
+      end
 
-    actual_inputs = Array(actual.expected_keys)
-    @missing_inputs = expected_inputs - actual_inputs
-    @extra_inputs = actual_inputs - expected_inputs
+      def failure_message
+        message = "expected #{actual} to #{type} #{expected_values.inspect}"
+        message += "\n\tmissing: #{missing}" if missing.any?
+        message += "\n\textra: #{extra}" if extra.any?
+        message
+      end
 
-    @missing_inputs.empty? && @extra_inputs.empty?
-  end
+      def valid?
+        missing.empty? && extra.empty?
+      end
 
-  failure_message do |actual|
-    message = "expected #{actual} to expect inputs #{expected_inputs.inspect}"
-    message += "\n\tmissing inputs: #{@missing_inputs}" if @missing_inputs
-    message += "\n\textra inputs: #{@extra_inputs}" if @extra_inputs
-    message
+      def missing
+        expected_values - actual_values
+      end
+
+      def extra
+        actual_values - expected_values
+      end
+    end
   end
 end
 
-# Custom matcher that implements promise_outputs
+# Custom matchers that implement expect_inputs, promise_outputs, organize_interactors
+# e.g. expect(described_class).to expect_inputs(:connection, :order)
 # e.g. expect(described_class).to promise_outputs(:request_logger)
-RSpec::Matchers.define :promise_outputs do |*expected_outputs|
-  match do |actual|
-    next false unless actual.respond_to?(:promised_keys)
-
-    actual_outputs = Array(actual.promised_keys)
-    @missing_outputs = expected_outputs - actual_outputs
-    @extra_outputs = actual_outputs - expected_outputs
-
-    @missing_outputs.empty? && @extra_outputs.empty?
-  end
-
-  failure_message do |actual|
-    message = "expected #{actual} to promise outputs #{expected_outputs.inspect}"
-    message += "\n\tmissing outputs: #{@missing_outputs}" if @missing_outputs
-    message += "\n\textra outputs: #{@extra_outputs}" if @extra_outputs
-    message
-  end
-end
-
-# Custom matcher that implements organize_interactors
-#
 # e.g. expect(described_class).to organize_interactors(SeparateIntoPackages, SendPackagesToSeko)
-RSpec::Matchers.define :organize_interactors do |*expected_interactors|
-  match do |actual|
-    actual_interactors = actual.organized
-    @missing_interactors = expected_interactors - actual_interactors
-    @extra_interactors = actual_interactors - expected_interactors
+[
+  %i[expect_inputs expected_keys],
+  %i[promise_outputs promised_keys],
+  %i[organize_interactors organized]
+].each do |type, meth|
+  RSpec::Matchers.define type do |*expected_values|
+    match do |actual|
+      next false unless actual.respond_to?(meth)
 
-    @missing_interactors.empty? && @extra_interactors.empty?
-  end
+      actual_values = Array(actual.send(meth))
 
-  failure_message do |actual|
-    message = "expected #{actual} to organize interactors #{expected_interactors.inspect}"
-    message += "\n\tmissing interactors: #{@missing_interactors}" if @missing_interactors
-    message += "\n\textra interactors: #{@extra_interactors}" if @extra_interactors
-    message
+      @contract_matcher = Interactify::RSpec::ContractMatcher.new(
+        actual,
+        expected_values,
+        actual_values,
+        type.to_s.gsub("_", " ")
+      )
+
+      @contract_matcher.valid?
+    end
+
+    failure_message do
+      @contract_matcher.failure_message
+    end
   end
 end
