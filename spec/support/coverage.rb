373c646b830788409caf141eb82fc8d246426fec
Mark Burns <markburns@users.noreply.github.com>
2023-12-29 14:32:24 +0000

    fixup rename wiring

--- spec/support/coverage.rb
+++ spec/support/coverage.rb
@@ -4,12 +4,12 @@ require "simplecov"
 
 SimpleCov.start do
   add_group "Sidekiq jobs" do |src_file|
-    src_file.project_filename =~ /lib\/interactify\/job/ || 
-      src_file.project_filename =~ /spec\/lib\/interactify\/job/
+    src_file.project_filename =~ %r{lib/interactify/job} ||
+      src_file.project_filename =~ %r{spec/lib/interactify/job}
   end
 
-  add_group "Wiring", "lib/interactify/interactor_wiring"
+  add_group "Wiring", "lib/interactify/wiring"
   add_group "RSpec matchers", "lib/interactify/rspec"
 
-  coverage_dir "coverage/#{ENV['RUBY_VERSION']}-#{ENV['APPRAISAL']}"
+  coverage_dir "coverage/#{ENV.fetch('RUBY_VERSION', nil)}-#{ENV.fetch('APPRAISAL', nil)}"
 end
