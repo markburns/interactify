b98f496e3683995e3f86b4354f109240c7ddf73c
Mark Burns <markburns@users.noreply.github.com>
2023-12-29 05:59:32 +0000

    add missing specs

--- spec/lib/interactify/rspec/matchers_spec.rb
+++ spec/lib/interactify/rspec/matchers_spec.rb
@@ -14,6 +14,14 @@ RSpec.describe "rspec matchers" do
       expect(k(:P)).not_to expect_inputs(:a, :b)
     end
 
+    it 'shows failure messages' do
+      expect { expect(k(:A)).to expect_inputs(:b) }.to raise_error do |error|
+        expect(error.message).to include("SomeNamespace::A to expect inputs [:b]")
+        expect(error.message).to include("missing inputs: [:b]")
+        expect(error.message).to include("extra inputs: [:a]")
+      end
+    end
+
     it "works with vanilla interactors without blowing up" do
       expect(k(:Vanilla)).not_to expect_inputs(:a)
     end
@@ -27,9 +35,32 @@ RSpec.describe "rspec matchers" do
       expect(k(:P)).to promise_outputs(:a, :b, :c)
     end
 
+    it 'shows failure messages' do
+      expect { expect(k(:A)).to promise_outputs(:b) }.to raise_error do |error|
+        expect(error.message).to include("SomeNamespace::A to promise outputs [:b]")
+        expect(error.message).to include("missing outputs: [:b]")
+        expect(error.message).to include("extra outputs: []")
+      end
+    end
     it "works with vanilla interactors without blowing up" do
       expect(k(:Vanilla)).not_to promise_outputs(:a)
     end
+  end
+
+  describe '#organize_interactors' do
+    it 'passes when the interactors are correct' do
+      expect(k(:O)).to organize_interactors(k(:A), k(:B))
+      expect(k(:O)).not_to organize_interactors(k(:B), k(:P))
+      expect(k(:O)).not_to organize_interactors(k(:A), k(:B), k(:P))
+    end
+    it 'shows failure messages' do
+      expect { expect(k(:O)).to organize_interactors(k(:B), k(:P)) }.to raise_error do |error|
+        expect(error.message).to include("SomeNamespace::O to organize interactors [#{k(:B)}, #{k(:P)}]")
+        expect(error.message).to include("missing interactors: [#{k(:P)}]")
+        expect(error.message).to include("extra interactors: [#{k(:A)}]")
+      end
+    end
+
   end
 
   module self::SomeNamespace
@@ -50,6 +81,11 @@ RSpec.describe "rspec matchers" do
     P = Class.new do
       include Interactify
       promise :a, :b, :c
+    end
+
+    O = Class.new do
+      include Interactify
+      organize A, B
     end
   end
 
