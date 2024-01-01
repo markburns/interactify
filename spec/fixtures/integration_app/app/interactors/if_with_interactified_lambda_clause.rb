94c0a7e238a7e307d657425d6e36b018ea2c4e0b
Mark Burns <markburns@users.noreply.github.com>
2024-01-01 12:05:52 +0000

    remove duplicate interactify definition in fixture

--- spec/fixtures/integration_app/app/interactors/if_with_interactified_lambda_clause.rb
+++ spec/fixtures/integration_app/app/interactors/if_with_interactified_lambda_clause.rb
@@ -4,7 +4,6 @@ module A
       include Interactify
       expect :some_flag_is_set, filled: false
 
-      SomeFlagIsSet = Interactify(&:some_flag_is_set)
       SomeFlagIsSet = Interactify do |c|
         c.some_flag_is_set
       end
